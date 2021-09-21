26d75c13b1444adf098cf54a08d964e2eac29fb5
Victor Jacon <74216766+Victor-Jacon@users.noreply.github.com>
2021-09-21 01:02:12 -0300

    reactflix - firebase config + firebase tests

--- parte6/src/pages/Page1.jsx
+++ parte6/src/pages/Page1.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import { React, useEffect, useState } from 'react'
+
+// [Firebase Config 5] Importamos o firebase usando desestruturação
+import { db, firebaseApp } from '../firebase'
+
+// [Firebase Config 6] Importamos os métodos para realizarmos consultas
+import { collection, doc, getDocs } from "firebase/firestore";
+
+// Components
 import styled, { createGlobalStyle } from 'styled-components'
 import VideoSingle from '../components/VideoSingle'
 import VideosList from '../components/VideosList'
 
 const Page1 = () => {
+
+  const [videosFirebase, setVideosFirebase] = useState([])
+
+  async function getVideos () {
+      const videosCol = collection(db, 'videos');
+      // console.log('videosCol'); console.log(videosCol)
+      const videosSnapshot = await getDocs(videosCol);
+      // console.log('videosSnapshot'); console.log(videosSnapshot)
+      const videosList = videosSnapshot.docs.map((doc) => doc.data());
+      console.log('videosList'); console.log(videosList)
+      return videosList
+    }
+
+  // [Firebase Testes A] Pegando referência de uma coleção do firebase
+  async function testeCollectionRef () {
+    const refVideoCollection = collection(db, 'videos')
+    console.log(refVideoCollection)
+  }
+
+  // [Firebase Testes b] Pegando referência de um documento do firebase
+  async function testeDocRef () {
+    const refVideoCollection = doc(db, 'videos', 'pxdGkvtbf3kTGmud2kqO')
+    console.log(refVideoCollection)
+  }
+
+  // [Firebase Testes c] Pegando referência de um documento do firebase
+  async function testeDocRef2 () {
+    const refVideoCollection = doc(db, 'videos/pxdGkvtbf3kTGmud2kqO')
+    console.log(refVideoCollection)
+  }
+
+
   return (
     <Container>
       <Header>
@@ -18,6 +58,10 @@ const Page1 = () => {
       <Footer>
         &copy; 2018
       </Footer>
+
+      <button onClick={() => testeDocRef2()}>
+        Ver valor videos
+      </button>
     </Container>
   )
 }
